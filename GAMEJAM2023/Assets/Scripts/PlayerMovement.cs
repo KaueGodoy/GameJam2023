6f25e84996fb5dc2ba252df83d3de000c44d20e5
Kaue Godoy <kauegodoy@live.com>
2023-01-30 22:55:22 -0300

    SimpleMovement

--- GAMEJAM2023/Assets/Scripts/PlayerMovement.cs
+++ GAMEJAM2023/Assets/Scripts/PlayerMovement.cs
@@ -4,18 +4,28 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Rigidbody2D rb;
-   
+    // components
+    private SpriteRenderer spriteRenderer;
+    private Rigidbody2D rb;
+    private BoxCollider2D boxCollider;
+
+    // grounded
+    [SerializeField] private LayerMask jumpableGround;
+
     // movement
     private float moveX = 0f;
     public float moveSpeed = 5f;
 
     // jump
-    public float jumpForce = 0f;
+    public float jumpForce = 4f;
 
+  
     void Start()
     {
-       
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
+
     }
 
     void Update()
@@ -30,9 +40,22 @@ public class PlayerMovement : MonoBehaviour
 
     void ProcessInput()
     {
+        // horizontal movement
         moveX = Input.GetAxisRaw("Horizontal");
 
-        if (Input.GetButtonDown("Jump"))
+        // sprite flip
+        if(moveX > 0f)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if(moveX < 0f)
+        {
+            spriteRenderer.flipX = true;
+        }
+
+
+        // jump
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rb.velocity = Vector2.up * jumpForce;
         }
@@ -41,5 +64,10 @@ public class PlayerMovement : MonoBehaviour
     void Move()
     {
         rb.velocity = new Vector2(moveX * moveSpeed, rb.velocity.y);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(boxCollider.bounds.min, boxCollider.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
     }
 }
