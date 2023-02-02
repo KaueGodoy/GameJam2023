4684ad5886f8897e12def35c65b259dc8d78d663
Kaue Godoy <kauegodoy@live.com>
2023-02-01 21:09:22 -0300

    InventorySystem

--- GAMEJAM2023/Assets/Scripts/Collectibles/Coin.cs
+++ GAMEJAM2023/Assets/Scripts/Collectibles/Coin.cs
@@ -1,13 +1,19 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Coin : MonoBehaviour, ICollectible
 {
+    public static event HandleCoinCollected OnCoinCollected;
+    public delegate void HandleCoinCollected(ItemData itemData);
+    public ItemData coinData;
+
     public void Collect()
     { 
         Debug.Log("Random collected");
         Destroy(gameObject);
+        OnCoinCollected?.Invoke(coinData);
         
     }
 
---
+++ GAMEJAM2023/Assets/Scripts/InventorySystem/InventoryItem.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+[Serializable]
+public class InventoryItem
+{
+    public ItemData itemData;
+    public int stackSize;
+
+    public InventoryItem(ItemData item) 
+    {
+        itemData = item;
+        AddToStack();
+    }
+   
+    public void AddToStack()
+    {
+        stackSize++;
+    }
+
+    public void RemoveFromStack()
+    {
+        stackSize--;
+    }
+
+}
