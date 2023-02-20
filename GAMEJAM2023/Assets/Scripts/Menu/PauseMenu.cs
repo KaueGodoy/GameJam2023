e1d46c9c1bcb1d2fe6ef6dfa4e6e76e9e20dd70d
Kaue Godoy <kauegodoy@live.com>
2023-02-20 16:49:26 -0300

    Fase3Fix

--- GAMEJAM2023/Assets/Scripts/Menu/PauseMenu.cs
+++ GAMEJAM2023/Assets/Scripts/Menu/PauseMenu.cs
@@ -43,7 +43,7 @@ public class PauseMenu : MonoBehaviour
     public void Reset()
     {
         Time.timeScale = 1f;
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         GameIsPaused = false;
     }
     public void ToMainMenu()
